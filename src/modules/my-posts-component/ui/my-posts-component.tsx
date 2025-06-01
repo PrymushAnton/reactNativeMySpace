7793a1844b9cee458f318d542c5c550cad0be341
Ilia Bulkin <bulkin20088@gmail.com>
2025-06-01 22:16:07 +0300

    create new hook for update posts, fix bug with updating post on My Posts page

--- src/modules/my-posts-component/ui/my-posts-component.tsx
+++ src/modules/my-posts-component/ui/my-posts-component.tsx
@@ -1,48 +1,16 @@
 import { View, Text } from "react-native";
+import { useEffect } from "react";
 import { PublicatedPost } from "../../main-page/ui/post";
-import { useEffect, useState } from "react";
-import { usePost } from "../../main-page/hooks/usePost";
-import { IBackendPost } from "../../main-page/types/post-info";
-import { IUserPostWithUser } from "../../main-page/types/post";
 import { useAuthContext } from "../../../modules/auth/context";
 import { styles } from "./my-posts-component.styles";
+import { useFetchPosts } from "../../main-page/hooks/useFetchPosts";
 
 export function MyPostsPage() {
-	const [posts, setPosts] = useState<IUserPostWithUser[]>([]);
 	const { user } = useAuthContext();
-	const { getPostsByUserId } = usePost();
-
-	function adaptPost(post: IBackendPost): IUserPostWithUser {
-		return {
-			id: post.id,
-			name: post.title ?? "",
-			description: post.text ?? "",
-			defaultTags: Array.isArray(post.tags) ? post.tags : [],
-			customTags: [],
-			image: Array.isArray(post.images) ? post.images : [],
-			likes: post.likes ?? 0,
-			views: post.views ?? 0,
-			link: post.link ?? "",
-			userId: post.userId,
-			user: post.user,
-		};
-	}
-
-	async function fetchPosts() {
-		if (!user?.id) return;
-		const response = await getPostsByUserId(user.id);
-
-		if (response.status === "success" && Array.isArray(response.data)) {
-			const adaptedPosts = response.data.map(adaptPost);
-			setPosts(adaptedPosts);
-		} else {
-			console.error("error load user posts", response);
-		}
-	}
+	const { posts, fetchPosts } = useFetchPosts();
 
 	useEffect(() => {
-		if (!user?.id) return;
-		fetchPosts();
+		fetchPosts(); 
 	}, [user?.id]);
 
 	return (
@@ -56,8 +24,8 @@ export function MyPostsPage() {
 			) : (
 				posts.map((post) => (
 					<PublicatedPost
-						id={post.id}
 						key={post.id}
+						id={post.id}
 						name={post.name}
 						text={post.description}
 						hashtags={[...post.defaultTags, ...post.customTags]}
@@ -65,7 +33,7 @@ export function MyPostsPage() {
 						user={post.user}
 						likes={post.likes ?? 0}
 						views={post.views ?? 0}
-						onRefresh={fetchPosts}
+						onRefresh={fetchPosts} 
 					/>
 				))
 			)}
